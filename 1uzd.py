31936e695ffb606b2d38542d92559ef9dd20bd50
DavisTarvids17 <davis.tarvids1@gmail.com>
2023-11-10 16:13:12 +0200

    2.variants

--- 1uzd.py
+++ 1uzd.py
@@ -14,15 +14,27 @@ cursor.execute('''
                )
                ''')
 
+# 3. Izveidot otru tabulu ar relāciju
 
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS pasutijumi(
+                    id INTEGER PRIMARY KEY,
+                    lietotaja_id INTEGER,
+                    produkta_nosaukums TEXT NOT NULL,
+                    daudzums INTEGER NOT NULL,
+                    FOREIGN KEY (lietotaja_id) REFERENCES lietotaji(id)
+               )
+               ''')
 
+# 4. Ievietot datus lietotāju tabulā
+#cursor.execute("INSERT INTO lietotaji (vards, uzvards, epasts) VALUES (?, ?, ?)",('Jānis', 'Bērziņš', 'janis.berzins@example.lv'))
+#cursor.execute("INSERT INTO lietotaji (vards, uzvards, epasts) VALUES (?, ?, ?)",('Alise', 'Liepiņa', 'alise.liepina@example.com'))
+# 5. Ievietot datus pasūtījuma tabulā
 
-
-
-
-
-
+cursor.execute("INSERT INTO pasutijumi (lietotaja_id, produkta_nosaukums, daudzums) VALUES (?, ?, ?)",(1, 'Dators', 2))
+cursor.execute("INSERT INTO pasutijumi (lietotaja_id, produkta_nosaukums, daudzums) VALUES (?, ?, ?)",(2, 'Televizors', 5))
 
 
 conn.commit()
 conn.close()
+
